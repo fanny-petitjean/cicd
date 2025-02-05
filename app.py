79e78d23bdfb87ed79e4be10c7d7a1c53d293c06
Fanny PETITJEAN <fannyy9934@gmail.com>
2025-02-05 11:27:10 +0100

    update ci

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def calendrier():
     return render_template('index.html', content="Page Calendrier")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000) 
