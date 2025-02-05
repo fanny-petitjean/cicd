3edec045889e7421bdc2a204639fc8307186426f
Fanny PETITJEAN <fannyy9934@gmail.com>
2025-02-05 17:19:21 +0100

    visual

--- back/app.py
+++ back/app.py
@@ -1,9 +1,10 @@
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# API externe à appeler (exemple)
+HOLIDAY_API_URL = 'https://date.nager.at/api/v3/PublicHolidays'
+
 METEO_API_URL = 'https://api.meteo.com/v1/weather'
 API_KEY = 'YOUR_API_KEY'
 
@@ -24,5 +25,19 @@ def meteo():
         # Retourner une erreur si l'appel échoue
         return jsonify({'error': 'API météo non disponible'}), 500
 
+@app.route('/api/calendrier')
+def calendrier():
+    # Récupérer les paramètres du formulaire
+    year = request.args.get('year', default=2025, type=int)
+    country_code = request.args.get('countryCode', default='FR', type=str).upper()  # Remplacer 'country' par 'countryCode'
+    
+    response = requests.get(f"{HOLIDAY_API_URL}/{year}/{country_code}")
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'API jours fériés non disponible'}), 500
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
